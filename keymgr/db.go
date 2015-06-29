96c1aecf17ca0dd9731f1fa2d6d7c3bf884823ef
Ishbir Singh <ishbir@ishbir.com>
2015-06-29 14:57:44 +0530

    Add key manager for managing private keys

---
+++ keymgr/db.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2015 Monetas.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package keymgr
+
+import (
+	"errors"
+)
+
+// dbInitSize is the size of the ImportedIDs and DerivedIDs slices.
+const dbInitSize = 10
+
+var (
+	// ErrUnknownKeyfileVersion  is returned when the version of the keyfile is
+	// unknown. Maybe we are using a newer keyfile with an older bmclient.
+	ErrUnknownKeyfileVersion = errors.New("unknown keyfile version")
+)
+
+// db is a representation of how data should be encoded.
+type db struct {
+	// Version is the version of key manager file.
+	Version int `json:"version"`
+
+	// MasterKey is used to derive all HD encryption and signing keys.
+	MasterKey *MasterKey `json:"masterKey"`
+
+	// ImportedIDs contains all IDs that weren't derived from the master key.
+	// This could include channels or addresses imported from PyBitmessage.
+	ImportedIDs []*PrivateID `json:"importedIDs"`
+
+	// DerivedIDs contains all IDs derived from the master key.
+	DerivedIDs []*PrivateID `json:"derivedIDs"`
+
+	// NewIDIndex contains the index of the next identity that will be derived
+	// according to BIP-BM01.
+	NewIDIndex uint32 `json:"newIDIndex"`
+}
+
+// init initializes the database struct.
+func (db *db) init() {
+	db.ImportedIDs = make([]*PrivateID, 0, dbInitSize)
+	db.DerivedIDs = make([]*PrivateID, 0, dbInitSize)
+}
+
+// checkAndUpgrade checks the version of the database and upgrades it if it
+// isn't the latest.
+func (db *db) checkAndUpgrade() error {
+	if db.Version != 1 {
+		return ErrUnknownKeyfileVersion
+	}
+	return nil
+}
---
+++ keymgr/doc.go
@@ -0,0 +1,35 @@
+// Copyright (c) 2015 Monetas.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// Package keymgr is a key manager for use with bmclient. However, it is meant
+// to be used independently and handles hierarchically deterministic keys,
+// imported keys (from PyBitmessage or other legacy systems), as well as
+// channels.
+//
+// The file format for storage is a JSON document encrypted with SalsaX20 and
+// MAC'd with Poly1305. The scheme used is byte compatible with secretbox from
+// NaCl. Currently, it supports encrypting the entire file containing identity
+// information (encryption and signing keys, required PoW constants, etc) and
+// master key.
+//
+// Future support for separately encrypting master key and signing keys is
+// planned. This could be useful for:
+// * Read only bmclient nodes that are intended to only receive and decrypt
+//   messages bound for them and are not likely to be used for sending messages.
+// * Security conscious users who might be worried about memory-reading malware.
+//   Note that even if we configure bmclient as such, the malware would still be
+//   able to get the user's private encryption key. By the very nature of
+//   Bitmessage, private encryption keys need to be in memory all the time.
+//
+// The contents of the encrypted file are arranged as such:
+// Nonce (24 bytes) || Salt for PBKDF2 (32 bytes) || Encrypted data
+//
+// Both the nonce and salt are re-generated each time the file needs to be
+// saved. This, along with a high number of PBKDF2 iterations (2^15), helps to
+// ensure that an adversary with access to the key file will have an extremely
+// hard time trying to bruteforce it. Rainbow tables will be useless.
+//
+// WARNING (again): The key manager is insecure against memory reading malware
+//                  and is at the mercy of Go's garbage collector.
+package keymgr
